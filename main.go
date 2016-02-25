eb6f31948f7ff5d55c2b9b30147d91ac88769843
Leo Correa <lcorr005@gmail.com>
2016-02-25 12:59:32 -0500

    Allow interaction with bender-bot

--- main.go
+++ main.go
@@ -17,6 +17,14 @@ func main() {
 	log.Println("system going online...")
 	bot := grot.NewBot()
 
+	bot.Hear(`kill all (humans|robots)`, func(res *grot.Response) {
+		if res.Matches[1] == "robots" {
+			res.Send("This is the worst kind of discrimination. The kind against me!")
+		} else {
+			res.Send("Now we're talking!")
+		}
+	})
+
 	if err := bot.Run(); err != nil {
 		log.Fatal(err)
 	}
